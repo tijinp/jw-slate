5b61bfe3cbf5c56ffcad41f265da57fd8af317c6
tijin <tijin.dev@gmail.com>
2022-03-24 12:05:25 +0530

    First Cut

---
+++ src/main/java/com/tijin/jw/JwSlateApplication.java
@@ -0,0 +1,13 @@
+package com.tijin.jw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwSlateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwSlateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tijin/jw/JwSlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tijin.jw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwSlateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
